b2a31d7fd654ed72acde5c6c48388f2d2d11c719
BakPeter <bak.pet@gmail.com>
2022-05-28 19:06:40 +0300

    Serilog, log to file added

--- UniversityWebApi/Controllers/StudentController.cs
+++ UniversityWebApi/Controllers/StudentController.cs
@@ -9,29 +9,50 @@ namespace UniversityWebApi.Controllers;
 public class StudentController : Controller
 {
     private readonly IStudentService _studentService;
+    private readonly ILogger<StudentController> _logger;
 
-    public StudentController(IStudentService studentService) => _studentService = studentService;
+    public StudentController(
+        IStudentService studentService,
+        ILogger<StudentController> logger)
+    {
+        _studentService = studentService;
+        _logger = logger;
+    }
 
     [HttpGet("{id}")]
-    public Task<GetStudentResultModel> Get([FromRoute] string id) =>
-        _studentService.GetStudent(new GetStudentRequestModel(id));
-
+    public Task<GetStudentResultModel> Get([FromRoute] string id)
+    {
+        _logger.LogInformation("HTTP GET, Get student by {id}", id);
+        return _studentService.GetStudent(new GetStudentRequestModel(id));
+    }
     [HttpGet]
-    public Task<GetStudentsResultModel> Get() => _studentService.GetStudents();
+    public Task<GetStudentsResultModel> Get()
+    {
+        _logger.LogInformation("HTTP GET, Get students");
+        return _studentService.GetStudents();
+    }
 
 
     [HttpPost]
-    public Task<CreateStudentResultModel> Post([FromForm] CreateStudentRequestModel dto) =>
-        _studentService.CreateStudent(dto);
-
+    public Task<CreateStudentResultModel> Post([FromForm] CreateStudentRequestModel dto)
+    {
+        _logger.LogInformation("HTTP POST, Create student with the parameters : {CreateStudentRequestModel}", dto);
+        return _studentService.CreateStudent(dto);
+    }
     [HttpDelete]
-    public Task<DeleteStudentResultModel> Delete([FromQuery] DeleteStudentRequestModel dto) =>
-        _studentService.DeleteStudentAsync(dto);
+    public Task<DeleteStudentResultModel> Delete([FromQuery] DeleteStudentRequestModel dto)
+    {
+        _logger.LogInformation("HTTP DELETE, DELETE student by {id}", dto.StudentId);
+        return _studentService.DeleteStudentAsync(dto);
+    }
 
     [HttpPut("{id}")]
-    public Task<UpdtateStudentResultModel> Put([FromRoute] string id, [FromBody] DataToUpdtae dto) =>
-        _studentService.UpdateStudentAsync(
+    public Task<UpdtateStudentResultModel> Put([FromRoute] string id, [FromBody] DataToUpdtae dto)
+    {
+        _logger.LogInformation("HTTP PUT, Update student by {id}, {dataToUpdate}", id, dto);
+        return _studentService.UpdateStudentAsync(
             new UpdateStudentRequestModel(StudentId: id, LastName: dto.LastName, FirstName: dto.FirstName));
+    }
 }
 
 public record DataToUpdtae(string LastName, string FirstName);
--- UniversityWebApi/Program.cs
+++ UniversityWebApi/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using StudentService.Services;
 using UniversityWebApi.Configuration;
 
@@ -8,9 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 //var settings = builder.Configuration.GetSection("<sectionName>").Get<Settings>();
 builder.Services.AddStudentServiceInfrastructureServices(
     new StudentService.Configurations.StudentServiceConfiguration());
+#endregion
+#region Serlog
+
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 #endregion
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
